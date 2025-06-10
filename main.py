5f652e4ba2916019f5ee82feb071c191a5d5d2bf
anshu1016 <arunshukla98710@gmail.com>
2025-06-10 16:51:43 +0530

    Model Trainer Code done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 import sys
@@ -37,7 +38,17 @@ if __name__ =='__main__':
         logging.info('Initiate the Data Transformation')
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
-        logging.info('the Data Transformation Completed')        
+        logging.info('the Data Transformation Completed')    
+
+
+        # Model Trainer
+        logging.info("Model Training stared")
+        model_trainer_config=ModelTrainerConfig(trainingPipelineConfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+   
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -13,7 +13,9 @@ ARTIFACT_DIR: str = 'Artifacts'
 FILE_NAME: str = 'NetworkData.csv'
 TRAIN_FILE_NAME = 'train.csv'
 TEST_FILE_NAME = 'test.csv'
-
+SCHEMA_FILE_PATH = os.path.join('data_schema','schema.yaml')
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
 
 
 
@@ -31,7 +33,6 @@ DATA_INGESTION_INGESTED_DIR:str = 'ingested'
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2
 
 
-SCHEMA_FILE_PATH = os.path.join('data_schema','schema.yaml')
 
 
 '''
@@ -61,5 +62,19 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     'n_neighbors': 3,
     'weights': 'uniform'
 }
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 
 
+"""
+Model Trainer related constant start with More Trainer VAR NAme
+
+"""
+
+MODEL_TRAINER_DIR_NAME: str = 'model_trainer'
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = 'trained_model'
+MODEL_TRAINER_TRAINED_MODEL_NAME: str= 'model.pkl'
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+TRAINING_BUCKET_NAME = "netwworksecurity"
