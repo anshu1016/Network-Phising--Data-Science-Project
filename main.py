89921a0a947fe454a19940817ff0b9f5bf9f83c8
anshu1016 <arunshukla98710@gmail.com>
2025-06-10 11:08:23 +0530

    Data Transformation Pipeline done according to Industry Standard

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 import sys
@@ -28,6 +29,15 @@ if __name__ =='__main__':
         datavalidationartifact = data_validation.initiate_data_validation()
         print(datavalidationartifact)
         logging.info('the Data Validation Completed')
+
+
+        # DATA TRANSFORMATION
+        dataTransformationConfig = DataTransformationConfig(trainingPipelineConfig)
+        data_transformation = DataTransformation(datavalidationartifact,dataTransformationConfig)
+        logging.info('Initiate the Data Transformation')
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info('the Data Transformation Completed')        
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from dataclasses import dataclass
 import os
-
+import numpy as np
 
 '''
 defining  common constant variable for trainig pipeline
 
 '''
-TARGET_COLUMN = 'RESULT'
+TARGET_COLUMN = 'Result'
 PIPELINE_NAME = 'NetworkSecurity'
 ARTIFACT_DIR: str = 'Artifacts'
 FILE_NAME: str = 'NetworkData.csv'
@@ -43,3 +43,23 @@ DATA_VALIDATION_VALID_DIR: str = 'validated'
 DATA_VALIDATION_INVALID_DIR: str = 'invalid'
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = 'drift_report'
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = 'report.yaml'
+
+
+
+'''
+Data Transformation related to constant start with Data Transformation Var Name
+'''
+
+DATA_TRANSFORMATION_DIR_NAME: str = 'data_transformation'
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = 'transformed'
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR : str = 'transformed_object'
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl" 
+
+# KNN IMputer to replace NAN values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    'missing_values': np.nan,
+    'n_neighbors': 3,
+    'weights': 'uniform'
+}
+
+
